3b3036d4913f6e1a8bf1c694597d323a481dd734
parteekcoder <parteekgoyal118@gmail.com>
2023-01-13 14:46:55 +0530

    5

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:t
 
 
 
-const PORT = 4000; // backend routing port
+const PORT = 3000; // backend routing port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
