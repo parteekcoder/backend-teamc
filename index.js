6a33d6ac81a7e0841b0ff88b7735f8c3cc319c07
parteekcoder <parteekgoyal118@gmail.com>
2023-01-13 23:57:51 +0530

    5

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const routesHandler = require('./routes/handler.js');
 const checkDepartment=require('./routes/checkDepart.js');
 const mongoose = require('mongoose');
+const cors=require('cors');
 require('dotenv/config');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 // app.use(express.static(`${__dirname}`+`../frontend/public`))
